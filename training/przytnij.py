ae4370666ace5534f801c3dc0775d5f4ff3ac898
pl313110 <pl313110@student.polsl.pl>
2025-05-26 12:32:00 +0200

    przycinanie zdjęć do kwadratu

--- training/przytnij.py
+++ training/przytnij.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 
 
 nazwa="15.jpg"
-x=430
-y=1480
-w=h=900
-# h=873
+# x=430
+# y=1480
+# w=h=900
+# # h=873
 
 sfoto="original/"+nazwa
 smask="mask/"+nazwa
@@ -15,12 +15,23 @@ smask="mask/"+nazwa
 foto=cv2.imread(sfoto)
 mask=cv2.imread(smask)
 
+# wymiary oryginałów
+h_img, w_img = foto.shape[:2]
+
+# bok kwadratu jako mniejszy z wymiarów
+side = min(h_img, w_img)
+
+# lewy-górny róg przycięcia (centrowanie)
+x = (w_img - side) // 2
+y = (h_img - side) // 2
+
+
 
 # Rysowanie prostokątów
 foto_with_rect = foto.copy()
 mask_with_rect = mask.copy()
-cv2.rectangle(foto_with_rect, (x, y), (x + w, y + h), (0, 255, 0), 2)  # Zielony prostokąt
-cv2.rectangle(mask_with_rect, (x, y), (x + w, y + h), (0, 255, 0), 2)
+cv2.rectangle(foto_with_rect, (x, y), (x + side, y + side), (0, 255, 0), 2)  # Zielony prostokąt
+cv2.rectangle(mask_with_rect, (x, y), (x + side, y + side), (0, 255, 0), 2)
 
 # Wyświetlanie obrazów z konturami
 # plt.figure(figsize=(10, 5))
@@ -34,9 +45,12 @@ plt.imshow(cv2.cvtColor(foto_with_rect, cv2.COLOR_BGR2RGB))
 
 plt.show()
 
-foto2 = foto[y:y+h, x:x+w]
-mask2 = mask[y:y+h, x:x+w]
+# foto2 = foto[y:y+h, x:x+w]
+# mask2 = mask[y:y+h, x:x+w]
 
+# przycinanie — tu zachowujemy foto2 i mask2
+foto2 = foto[y:y+side, x:x+side]
+mask2 = mask[y:y+side, x:x+side]
 
 plt.imshow(foto2)
 
