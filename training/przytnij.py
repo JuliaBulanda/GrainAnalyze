cbca6b3d81d4ca73b622915ba1fc03dab95c96f5
pl313110 <pl313110@student.polsl.pl>
2025-05-26 09:19:25 +0200

    przygotowanie do przycięcia zdjęć szkoleniowych

--- training/przytnij.py
+++ training/przytnij.py
@@ -7,8 +7,11 @@ y=1609
 w=884
 h=873
 
-foto=cv2.imread("original/"+nazwa)
-mask=cv2.imread("mask/"+nazwa)
+sfoto="original/"+nazwa
+smask="mask/"+nazwa
+
+foto=cv2.imread(sfoto)
+mask=cv2.imread(smask)
 
 # print(foto)
 #
@@ -19,5 +22,5 @@ mask=cv2.imread("mask/"+nazwa)
 foto2=foto[x:x+h][y:y+h]
 mask2=mask[x:x+h][y:y+h]
 
-cv2.imwrite("orginal/"+nazwa, foto2)
-cv2.imwrite("mask/"+nazwa, mask2)
+cv2.imwrite(sfoto, foto2)
+cv2.imwrite(smask, mask2)
